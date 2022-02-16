318fb68608966c7e6378874e5520206a35712ecc
unknown <ajaymaurya3494@gmail.com>
2022-02-16 13:17:08 +0530

    first commit

---
+++ src/main/java/com/example/demo/Demo1321AugGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1321AugGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1321AugGitApplication.class, args);
+	}
+
+}
